8766ba216785a834867bf7e9a6defb5bf82c3502
Vaibhav Surapaneni <vaibhavsurapaneni1979@gmail.com>
2025-03-09 16:44:22 -0400

    Task 2 - Employee Cards Dynamic Addition

---
+++ cc_13.js
@@ -0,0 +1,42 @@
+//Task 2 - Employee Cards Dynamic Addition
+function createEmployeeCard(name, position){
+    //Getting the main container to place employee cards
+    let divEmpContainer = document.getElementById('employeeContainer');
+    
+    //Creating a new card for the employee
+    const employeeCard = document.createElement('div');
+    employeeCard.setAttribute('class','employee-card');
+    
+    //Adding the employee's name to the card
+    const empName = document.createElement('h2');
+    empName.setAttribute('class', 'employee-name');
+    empName.textContent = name;
+    employeeCard.append(empName);
+    
+    //Adding the employee's position to the card
+    const empPosition = document.createElement('p');
+    empPosition.setAttribute('class', 'employee-position');
+    empPosition.textContent = position;
+    employeeCard.append(empPosition);
+
+    //Adding a button to remove the employee card
+    const removeBtn = document.createElement('button');
+    removeBtn.setAttribute('class', 'remove-btn');
+    removeBtn.textContent = 'Remove';
+    employeeCard.append(removeBtn); 
+}
+
+//When the webpage loads, the initial employee cards are added and the bulk styles are applied to all the cards
+document.addEventListener('DOMContentLoaded', function (){
+    createEmployeeCard('Doug Webster', 'Chief Technical Officer');
+    createEmployeeCard('Sherrie Booker', 'Chief Information Officer');
+    createBulkEmpStyleUpdate();
+})
+
+//Added in case there was an add button which I thought needed to be included. 
+//If the add employee button is not required on the HTML page, this function can be removed.
+//When "Add Employee" button is clicked, a new card is added and the styles are updated for all the cards
+document.getElementById('addEmployeeBtn').addEventListener('click', () => {
+    const currentCard = createEmployeeCard('Alice Hudson', 'Manager');
+    setEmployeeCardStyle(currentCard);
+})
